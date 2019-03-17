dc4d2da7c5aab9dfe1c9d2af34c48718d941fdf1
Боярских Никита Игоревич <N02@yandex.ru>
2019-03-17 10:45:04 +0300

    Update backend/project/apps/core/models/element.py
    Co-Authored-By: eyugavrilova <43515875+eyugavrilova@users.noreply.github.com>

--- backend/project/apps/core/models/element.py
+++ backend/project/apps/core/models/element.py
@@ -25,7 +25,7 @@ class Element(models.Model):
     creator_id = models.ForeignKey(User, on_delete=models.SET_NULL, name='Создатель ЛЭ', blank=True, null=True)
     element_type = models.CharField(
         'Тип элемента', max_length=30, choices=[(tag, tag.value) for tag in TypeChoice]
-    )  #источник, земля, генератор, анализатор, И, ИЛИ, НЕ, др.
+    )
     array_of_inputs = ArrayField(models.CharField('Массив имён входов', max_length=255))
     array_of_outputs = ArrayField(models.CharField('Массив имён выходов', max_length=255))
     time = models.IntegerField('Время работы', default=0)
